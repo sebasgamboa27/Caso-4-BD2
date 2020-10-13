0b445a3bf29574a63a6a16cdbc490610dcf06305
SebasG <gamboasebas@gmail.com>
2020-10-13 12:09:44 -0600

    connection to mongo added

--- caso4API/server/indexMongo.js
+++ caso4API/server/indexMongo.js
@@ -2,36 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const {MongoClient} = require('mongodb');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/articulos', {useNewUrlParser: true});
 
 app.listen(3050, function () {
   console.log('MongDB server API listening on port 3050!');
 });
 
-async function main(){
-  const uri = "mongodb://localhost:27017/articulos";
-  const client = new MongoClient(uri);
-
-  try {
-      await client.connect();
-
-      await  listDatabases(client);
-
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
-}
-
-main().catch(console.error);
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to DB!')
+  // we're connected!
+});
